29c8589179a131b8e897a19e25dad071b4690160
Emalyanov535 <nouctanch777093@gmail.com>
2024-05-04 14:43:29 +0400

    Feature: solved test task

--- src/FlightData.java
+++ src/FlightData.java
@@ -1,8 +1,76 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FlightData {
-    private ArrayList<Ticket> tickets;
-    public ArrayList<Ticket> getTickets() {
+    private List<Ticket> tickets;
+
+    public List<Ticket> getTickets() {
         return tickets;
+    }
+
+    public Map<String, String> getMinTimeBetweenCities() {
+        return tickets.stream()
+                .filter(ticket -> ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV"))
+                .collect(Collectors.groupingBy(
+                        Ticket::getCarrier,
+                        Collectors.collectingAndThen(
+                                Collectors.minBy(Comparator.comparing(this::calculateFlightDuration)),
+                                minTicket -> minTicket.map(this::calculateFlightDuration)
+                                        .map(this::formatDuration)
+                                        .orElse("")
+                        )
+                ));
+    }
+
+    private String formatDuration(Duration duration) {
+        long hours = duration.toHours();
+        long minutes = duration.toMinutesPart();
+        return hours + " ч " + minutes + " мин";
+    }
+
+    private Duration calculateFlightDuration(Ticket ticket) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yy H:mm"); // Используем H:mm для одиночного часа
+        LocalDateTime departureDateTime = LocalDateTime.parse(ticket.getDeparture_date() + " " + ticket.getDeparture_time(), dateTimeFormatter);
+        LocalDateTime arrivalDateTime = LocalDateTime.parse(ticket.getArrival_date() + " " + ticket.getArrival_time(), dateTimeFormatter);
+        return Duration.between(departureDateTime, arrivalDateTime);
+    }
+
+    public double getDiffBetweenAvgAndMedian(){
+        List<Ticket> vvoToTlvTickets = tickets.stream()
+                .filter(ticket -> ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV"))
+                .toList();
+
+        List<Integer> prices = vvoToTlvTickets.stream()
+                .map(Ticket::getPrice)
+                .toList();
+
+        double averagePrice = prices.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+
+        double medianPrice = calculateMedian(prices);
+
+        return averagePrice - medianPrice;
+    }
+
+    private double calculateMedian(List<Integer> prices) {
+        List<Integer> sortedPrices = new ArrayList<>(prices);
+        sortedPrices.sort(Integer::compareTo);
+
+        int size = sortedPrices.size();
+        if (size % 2 == 1) {
+            return sortedPrices.get(size / 2);
+        } else {
+            int midIndex1 = size / 2 - 1;
+            int midIndex2 = size / 2;
+            return (sortedPrices.get(midIndex1) + sortedPrices.get(midIndex2)) / 2.0;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,28 @@ import com.google.gson.Gson;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+
 
 public class Main {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\Admin\\Downloads\\tickets.json";
-
 
         try (FileReader reader = new FileReader(filePath)) {
             Gson gson = new Gson();
 
             FlightData flightData = gson.fromJson(reader, FlightData.class);
 
+            Map<String, String> minTimeBetweenCities = flightData.getMinTimeBetweenCities();
+
+            for (Map.Entry<String, String> entry : minTimeBetweenCities.entrySet()) {
+                System.out.println("Авиаперевозчик: " + entry.getKey());
+                System.out.println("Минимальное время полета: " + entry.getValue());
+                System.out.println();
+            }
+
+            System.out.println("Разница между средней ценой и медианой: " + flightData.getDiffBetweenAvgAndMedian());
 
         } catch (IOException e) {
             e.printStackTrace();
